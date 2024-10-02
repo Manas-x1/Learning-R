ac3b0868b29604f972f68170ea0df48533c4d462
Manas Upadhayay <manas9399upadhyay@gmail.com>
2024-10-02 13:56:28 +0000

    We started today ;D

---
+++ Basics/Datatypes.R
@@ -0,0 +1,25 @@
+#hello person, i hope you are doing well.
+#today we will be talking about datatypes
+
+#there are 5 datatypes in R
+#1. numeric
+numeric_var <- 3.14
+print(numeric_var)
+
+#2. character
+character_var <- "hello world"
+print(character_var)
+
+#3. logical
+logical_var <- TRUE
+print(logical_var)
+
+#4. integer
+integer_var <- 10
+print(integer_var)
+
+#5. complex
+complex_var <- 1 + 2i
+print(complex_var)
+
+#these are the most common datatypes in R that you will encounter.
---
+++ Basics/Startingfile.R
@@ -0,0 +1,25 @@
+#comment are ignored (you get me :D, haha)
+#anyways we begin with the basics first and well grab things as we go along
+#the print function prints the argument to the console
+print("Hello World")
+
+#making a variable and doing some math now , uknow how we do it
+
+addition <- 2 + 2
+print(addition)
+subtraction <- 2 - 2
+print(subtraction)
+multiplication <- 2 * 2
+print(multiplication)
+division <- 2 / 2
+print(division)
+power <- 2^99
+print(power)
+
+#doing some more math
+a <- 2
+b <- 3
+c <- a + b
+print(c)
+#ofc it works with variables too, i love you alpha and beta and fuck you gamma.
+#thats enough for now....see ya in the next file
